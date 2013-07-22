880e0fc6682d23597bf1862217f4afd41f3293e1
black apple <wenzhou.1991@163.com>
2013-07-22 16:48:14 +0800

    Initial Commit

---
+++ MyPano/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyPano
+//
+//  Created by icreative-mini on 13-7-22.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
